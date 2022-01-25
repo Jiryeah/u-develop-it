54536d8e2777a8962e46439f68ad62635b71a581
Chidori <business.faissal@outlook.com>
2022-01-25 01:03:01 -0600

    add CRUD routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require(`mysql2`);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require(`./utils/inputCheck`)
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -23,17 +24,40 @@ const db = mysql.createConnection(
 
 //* db object is using the query method to run the SQL query and executes
 //* the callback with all the resulting rows that match the query
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//   console.log(rows);
-// });
+//* also, the asterisk means 'all columns'.
+// Get all candidates
+app.get(`/api/candidates`, (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      //* 500 is a server error
+      res.status(500).json({ error: err.message });
+    }
+    res.json({
+      message: `success`,
+      data: rows
+    });
+  });
+});
 
 // GET a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(row);
-// });
+app.get(`/api/candidate/:id`, (req,res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      //* 400 notifies the client that their request wasn't accepted & to try a different request.
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: `success`,
+      data: row
+    });
+  });
+});
 
 // Query for Delete Operation
 //* '?' denotes a placeholder. This is now a 'prepared statement' which
@@ -41,13 +65,57 @@ const db = mysql.createConnection(
 //*  values in place of the placeholder.
 //* Another reason to use a placeholder in SQL query is to block
 //* a SQL injection attack, which replaces the clients user var
-//* & inserts alternate commands that could reveal or destroy the db
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+//* & inserts alternate commands that could reveal or destroy the db.
+app.delete(`/api/candidate/:id`, (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: `Candidate not found`
+      });
+    } else {
+      res.json({
+        message: `deleted`,
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Create a candidate
+//* post method used to insert candidate in candidates table.
+//* req.body object used to populate candidates data.
+//* object destructuring used to pull body property out of the request object.
+app.post(`/api/candidate`, ({ body }, res) => {
+  const errors = inputCheck(body, `first_name`, `last_name`, `industry_connected`)
+  if (errors) {
+    res.status(400).json({ error: errors});
+    return;
+  }
+
+  //* four placeholders were used for the corresponding columns holding the values.
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  //* four placeholders must match the four values in params, so we must use an array.
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
 
 // Create a candidate
 //* four placeholders were used for the corresponding columns holding the values.
