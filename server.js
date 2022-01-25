ce0ddc0954d51cfa0f9166b79ab5bbd7352d0266
Chidori <business.faissal@outlook.com>
2022-01-25 00:09:18 -0600

    created querys for: candidate creation, candidate deletion, and single candidate retrieval

--- server.js
+++ server.js
@@ -21,19 +21,47 @@ const db = mysql.createConnection(
   console.log(`Connected to the election database.`)
 );
 
-// db object is using the query method to run the SQL query and executes
-// the callback with all the resulting rows that match the query
+//* db object is using the query method to run the SQL query and executes
+//* the callback with all the resulting rows that match the query
 // db.query(`SELECT * FROM candidates`, (err, rows) => {
 //   console.log(rows);
 // });
 
 // GET a single candidate
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(row);
-});
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// Query for Delete Operation
+//* '?' denotes a placeholder. This is now a 'prepared statement' which
+//* can execute the same SQL statements repeatedly using different
+//*  values in place of the placeholder.
+//* Another reason to use a placeholder in SQL query is to block
+//* a SQL injection attack, which replaces the clients user var
+//* & inserts alternate commands that could reveal or destroy the db
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+//* four placeholders were used for the corresponding columns holding the values.
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+              // VALUES (?,?,?,?)`;
+//* four placeholders must match the four values in params, so we must use an array.
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
